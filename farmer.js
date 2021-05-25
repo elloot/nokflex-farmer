d4cdf2307cbe4eccc7de50a57f51498d2dce1e62
elloot <elliot.duchek@gmail.com>
2021-05-25 10:14:53 +0200

    fix farmer

--- farmer.js
+++ farmer.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright');
 require('dotenv').config();
 
 (async () => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: false, slowMo: 100 });
   const page = await browser.newPage();
   await page.goto('https://nokflex.nok.se');
   await page.fill('#login-input', process.env.EMAIL);
@@ -12,19 +12,22 @@ require('dotenv').config();
   // await page.keyboard.press('Enter');
   // await page.keyboard.press('Enter');
   await page.click('#next-button');
-  await page.click('#next-button');
   await page.click('.user-button-container :nth-child(2)');
   await page.click('button.v-btn.theme--light.primary');
   await page.click('a:not([Logo])');
+  await page.goto('https://nokflex.nok.se');
 
-  for (let i = 0; i < 5; i) {
-    await page.fill('#login-input', 'elliot.duchek@gmail.com');
+  for (let i = 0; i < 5; i++) {
+    await page.fill('#login-input', process.env.EMAIL);
+    await page.click('#next-button');
     await page.fill('#password-input', process.env.PASS);
     await page.click('#next-button');
-    await page.click('button.Button.Button--full');
+    // await page.click('button.Button.Button--full');
     await page.click('.user-button-container :nth-child(2)');
     await page.click('button.v-btn.theme--light.primary');
     await page.click('a:not([Logo])');
+    await page.waitForTimeout(1000);
+    await page.goto('https://nokflex.nok.se');
     await page.waitForTimeout(1000);
   }
   await browser.close();
